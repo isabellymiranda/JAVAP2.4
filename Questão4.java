15db5bc0a6990cca6f8d48950bbf8476cadb89ff
Isabelly Miranda <contatoisabellymiranda@gmail.com>
2022-06-06 17:15:09 +0000

    envio

---
+++ Questão4.java
@@ -0,0 +1,8 @@
+public class Questão4 {
+    public static void main(String[]args){
+	
+        for(int i=38 ; i<=1000 ; i++){
+                System.out.println(i);
+            }
+        }
+    }
